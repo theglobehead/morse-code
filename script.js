5a32b1a2b937f91e23909ce7d2b4796014037d08
Valters Vieško <76264011+theglobehead@users.noreply.github.com>
2023-01-20 13:29:26 +0200

    feat: added most of html and js

---
+++ script.js
@@ -0,0 +1,93 @@
+let $inputModeSelect = $("#input-mode-select")
+let $inputTextarea = $("#input-textarea")
+let $outputTextarea = $("#output-textarea")
+let $convertButton = $("#convert-button")
+
+let inputMode = $inputModeSelect.val() 
+
+let textToMorseDict = {
+    "a": "._",
+    "b": "_...",
+    "c": "_._.",
+    "d": "_..",
+    "e": ".",
+    "f": ".._.",
+    "g": "__.",
+    "h": "....",
+    "i": "..",
+    "j": ".___",
+    "k": "_._",
+    "l": "._..",
+    "m": "__",
+    "n": "_.",
+    "o": "___",
+    "p": ".__.",
+    "q": "__._",
+    "r": "._.",
+    "s": "...",
+    "t": "_",
+    "u": ".._",
+    "v": "..._",
+    "w": ".__",
+    "x": "_.._",
+    "y": "_.__",
+    "z": "__..",
+    "0": "_____",
+    "1": ".____",
+    "2": "..___",
+    "3": "...__",
+    "4": "...._",
+    "5": ".....",
+    "6": "_....",
+    "7": "__...",
+    "8": "___..",
+    "9": "____.",
+    " ": "/",
+}
+
+function setInputMode(mode){
+    inputMode = mode
+}
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+
+function convertInput(){
+    let inputText = $inputTextarea.val()
+    let result = ""
+
+    if(inputMode == "text"){
+        result = textToMorse(inputText)
+    }else{
+        result = morseToText(inputText)
+    }
+
+    $outputTextarea.text(result)
+}
+
+function morseToText(text){
+    result = ""
+
+    text = text.split(" ")
+
+    for(char of text){
+        if(getKeyByValue(textToMorseDict, char) != undefined){
+            result += getKeyByValue(textToMorseDict, char)
+        }
+    }
+
+    return result
+}
+
+function textToMorse(text){
+    result = ""
+
+    for(char of text){
+        if(textToMorseDict[char] != undefined){
+            result += textToMorseDict[char] + " "
+        }
+    }
+
+    return result
+}
